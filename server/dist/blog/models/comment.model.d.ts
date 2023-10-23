d0d18475ef90aad1a86a6c0369a66d0528101bd1
Sergei Martynov <48458112+Gerald231109@users.noreply.github.com>
2023-10-23 03:19:31 +0300

    Delete server directory

--- server/dist/blog/models/comment.model.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'sequelize-typescript';
-import { Article } from './article.model';
-export declare class Comment extends Model {
-    title: string;
-    text: string;
-    ArticleId: number;
-    article: Article;
-}
--- server/dist/blog/services/analytics.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Article } from "../models/article.model";
-export declare class AnalyticsService {
-    private articleModel;
-    constructor(articleModel: typeof Article);
-    findAllByDate(dateOne: string, dateTwo: string): Promise<Article[]>;
-}
